c45cf805eb5c526c3172aab254ee5a150ba52eca
MohGanji <mohammad-ganji@outlook.com>
2017-02-03 23:11:57 +0330

    added Making Anagrams

---
+++ MakingAnagrams.cpp
@@ -0,0 +1,30 @@
+// you can find the question here:
+// https://www.hackerrank.com/challenges/ctci-making-anagrams
+
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string a, b;
+    cin >> a >> b;
+    
+    int lettersA[26] = {0}, lettersB[26] = {0};
+    
+    
+    for (int i = 0 ; i < a.length() ; i++){
+        lettersA[int(a[i] - 'a')]++;
+    }
+    for (int i = 0 ; i < b.length() ; i++){
+        lettersB[int(b[i] - 'a')]++;
+    }
+    int ans = 0;
+    for (int i = 0 ; i < 26 ; i++){
+        ans += abs(lettersB[i] - lettersA[i]);
+    }
+    cout << ans << endl;
+    
+    
+    return 0;
+}
